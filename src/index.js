e9418e80921b21ceaa224b9aec51b51fdb8f4f9a
Mateus Durães <mduraes1994@gmail.com>
2019-08-26 02:38:03 -0300

    Install redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+function reducer(state = [], action) {
+  switch (action.type) {
+    case '@tech/add':
+      return [...state, action.payload]
+    case '@tech/initialize':
+      return [...state, ...action.payload]
+    default:
+      return state
+  }
+}
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
